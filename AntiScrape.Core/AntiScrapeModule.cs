3914517d13c114e638493153e69887bd6dd7af62
SneakyBrian <SneakyBrian@gmail.com>
2012-12-11 23:08:16 +0000

    Making module more testable

--- AntiScrape.Core/AntiScrapeModule.cs
+++ AntiScrape.Core/AntiScrapeModule.cs
@@ -14,17 +14,40 @@ using UnityConfiguration;
 using AntiScrape.Core.IoC;
 using AntiScrape.Core.Interfaces;
 using System.Security.Cryptography;
+using AntiScrape.Support;
 
 namespace AntiScrape.Core
 {
     public class AntiScrapeModule : IHttpModule
     {
         private IDataStorage _storage;
-        private byte[] saltBytes = Encoding.Unicode.GetBytes(AntiScrapeConfiguration.Settings.ClassNameSalt);
+        private readonly byte[] _saltBytes;
+        private readonly AntiScrapeConfiguration _settings;
+        private readonly Func<object, HttpApplicationBase> _getApplicationBase;
         
+        public AntiScrapeModule()
+            : this(AntiScrapeConfiguration.Settings, 
+                    obj => new HttpApplicationWrapper(obj as HttpApplication))
+        { }
+
+        public AntiScrapeModule(AntiScrapeConfiguration settings, 
+                                Func<object, HttpApplicationBase> getApplicationBase)
+        {
+            _settings = settings;
+
+            _getApplicationBase = getApplicationBase;
+
+            _saltBytes = Encoding.Unicode.GetBytes(settings.ClassNameSalt);
+        }
+
         public void Dispose() { }
 
         public void Init(HttpApplication context)
+        {
+            Init(_getApplicationBase(context));
+        }
+
+        public void Init(HttpApplicationBase context)
         {
             Debug.WriteLine("AntiScrapeModule.Init");
 
@@ -46,11 +69,13 @@ namespace AntiScrape.Core
         {
             Debug.WriteLine("AntiScrapeModule.OnPostMapRequestHandler");
 
-            HttpContext context = ((HttpApplication)sender).Context;
-            Page page = HttpContext.Current.CurrentHandler as Page;
+            var application = _getApplicationBase(sender);
+
+            var context = application.Context;
+            var page = context.CurrentHandler as Page;
             if (page != null)
             {
-                page.PreRenderComplete += new EventHandler(OnPreRenderComplete);
+                page.PreRenderComplete += OnPreRenderComplete;
             }
         }
 
@@ -58,45 +83,50 @@ namespace AntiScrape.Core
         {
             Debug.WriteLine("AntiScrapeModule.OnPreRenderComplete");
 
-            Page page = (Page)sender;
+            Page page = sender as Page;
 
-            var data = new List<byte>();
+            if (page != null)
+            {
+                var data = new List<byte>();
 
-            data.AddRange(saltBytes);
-            data.AddRange(Encoding.Unicode.GetBytes(page.AppRelativeVirtualPath));
+                data.AddRange(_saltBytes);
+                data.AddRange(Encoding.Unicode.GetBytes(page.AppRelativeVirtualPath));
 
-            var sha1 = new SHA1Managed();
+                byte[] hash;
+                using (var sha1 = new SHA1Managed())
+                {
+                    hash = sha1.ComputeHash(data.ToArray());
+                }
 
-            var hash = sha1.ComputeHash(data.ToArray());
+                var className = HttpServerUtility.UrlTokenEncode(hash);
 
-            var className = HttpServerUtility.UrlTokenEncode(hash);
+                Debug.WriteLine(string.Format("class name: {0}", className));
 
-            Debug.WriteLine(string.Format("class name: {0}", className));
+                var linkUri = new Uri(_settings.HoneypotRelativeUrl, UriKind.Relative);
 
-            var linkUri = new Uri(AntiScrapeConfiguration.Settings.HoneypotRelativeUrl, UriKind.Relative);
+                Debug.WriteLine(string.Format("Link Uri: {0}", linkUri));
 
-            Debug.WriteLine(string.Format("Link Uri: {0}", linkUri));
+                //add css for link
+                page.Header.Controls.Add(new LiteralControl(string.Format("<style type=\"text/css\">.{0} {{ display: none; }}</style>", className)));
 
-            //add css for link
-            page.Header.Controls.Add(new LiteralControl(string.Format("<style type=\"text/css\">.{0} {{ display: none; }}</style>", className)));
-
-            //add link
-            page.Form.Controls.Add(new LiteralControl(string.Format("<a class=\"{0}\" href=\"{1}\">{1}</a>", className, linkUri)));
+                //add link
+                page.Form.Controls.Add(new LiteralControl(string.Format("<a class=\"{0}\" href=\"{1}\">{1}</a>", className, linkUri)));
+            }
         }
 
         void OnBeginRequest(object sender, EventArgs e)
         {
             Debug.WriteLine("AntiScrapeModule.OnBeginRequest");
 
-            var application = sender as HttpApplication;
+            var application = _getApplicationBase(sender);
 
             if (application != null)
             {                
-                if (application.Context.Request.Url.AbsolutePath.Contains(AntiScrapeConfiguration.Settings.HoneypotRelativeUrl))
+                if (application.Context.Request.Url.AbsolutePath.Contains(_settings.HoneypotRelativeUrl))
                 {
                     Debug.WriteLine(string.Format("Request.Url.AbsolutePath '{0}' contains '{1}'", 
                                         application.Context.Request.Url.AbsolutePath, 
-                                        AntiScrapeConfiguration.Settings.HoneypotRelativeUrl));
+                                        _settings.HoneypotRelativeUrl));
 
                     _storage.StoreScrapingRequest(application.Context.Request);
                 }
@@ -107,9 +137,9 @@ namespace AntiScrape.Core
 
                 var content = "<html><head><title></title></head><body></body></html>";
 
-                if(!string.IsNullOrWhiteSpace(AntiScrapeConfiguration.Settings.ContentVirtualPath))
+                if(!string.IsNullOrWhiteSpace(_settings.ContentVirtualPath))
                 {
-                    var contentPath = application.Context.Server.MapPath(AntiScrapeConfiguration.Settings.ContentVirtualPath);
+                    var contentPath = application.Context.Server.MapPath(_settings.ContentVirtualPath);
 
                     if (File.Exists(contentPath))
                     {
@@ -117,7 +147,7 @@ namespace AntiScrape.Core
                     }
                 }
 
-                var action = AntiScrapeConfiguration.Settings.Action;
+                var action = _settings.Action;
 
                 Debug.WriteLine(string.Format("AntiScrapeConfiguration.Settings.Action = {0}", action));
 
@@ -127,7 +157,7 @@ namespace AntiScrape.Core
 
                         var rng = new Random();
 
-                        var delay = rng.Next(AntiScrapeConfiguration.Settings.MinDelay, AntiScrapeConfiguration.Settings.MaxDelay);
+                        var delay = rng.Next(_settings.MinDelay, _settings.MaxDelay);
 
                         Debug.WriteLine(string.Format("Waiting {0}", delay));
 
@@ -139,9 +169,9 @@ namespace AntiScrape.Core
 
                     case AntiScrapeAction.Error:
 
-                        Debug.WriteLine(string.Format("AntiScrapeConfiguration.Settings.ErrorCode = {0}", AntiScrapeConfiguration.Settings.ErrorCode));
+                        Debug.WriteLine(string.Format("AntiScrapeConfiguration.Settings.ErrorCode = {0}", _settings.ErrorCode));
 
-                        application.Context.Response.StatusCode = (int)AntiScrapeConfiguration.Settings.ErrorCode;
+                        application.Context.Response.StatusCode = (int)_settings.ErrorCode;
                         application.Context.Response.SuppressContent = true;
                         application.Context.Response.End();
 
---
+++ AntiScrape.Support/HttpApplicationWrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace AntiScrape.Support
+{
+    public class HttpApplicationWrapper : HttpApplicationBase
+    {
+        private readonly HttpApplication _application;
+
+        public event EventHandler BeginRequest;
+        public event EventHandler PostMapRequestHandler;
+
+        public override HttpContext Context { get { return _application.Context; } }
+        
+        public HttpApplicationWrapper(HttpApplication application)
+        {
+            _application = application;
+
+            _application.BeginRequest += application_BeginRequest;
+            _application.PostMapRequestHandler += application_PostMapRequestHandler;            
+        }
+
+        void application_PostMapRequestHandler(object sender, EventArgs e)
+        {
+            var handler = PostMapRequestHandler;
+            if (handler != null)
+                handler(sender, e);
+        }
+
+        void application_BeginRequest(object sender, EventArgs e)
+        {
+            var handler = BeginRequest;
+            if (handler != null)
+                handler(sender, e);
+        }
+    }
+}
